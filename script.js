b9aa2ad28113e570b668ea1937da018995b2a05c
Markus Dreyer <markus.a.dreyer@gmail.com>
2018-03-06 18:20:24 +0100

    nothing much

---
+++ script.js
@@ -0,0 +1,22 @@
+var today = new Date();
+var hourNow = today.getHours();
+var minuteNow = ("0" + today.getMinutes()).slice(-2);
+var greetingText;
+
+if (hourNow >= 18) {
+    greetingText = "GOOD EVENING";
+} else if (hourNow >= 12) {
+    greetingText = "GOOD AFTERNOON";
+} else if (hourNow >= 0) {
+    greetingText = "GOOD MORNING";
+}
+else {
+    greetingText = "WELCOME";
+}
+
+
+
+function greeting() {
+    document.getElementById("text").innerHTML = greetingText;
+    document.getElementById("timeOfDay").innerHTML = hourNow + ":" + minuteNow;
+}
